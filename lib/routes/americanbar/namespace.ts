8df3a5fc70021d62ad498e37b0ed6515d77e8dd0
XruiDD <48876676+XruiDD@users.noreply.github.com>
2025-09-18 16:29:41 +0800

    refactor(route) remove getPuppeteerPageNoUa and American Bar Association route

--- lib/routes/americanbar/namespace.ts
+++
@@ -1,8 +0,0 @@
-import type { Namespace } from '@/types';
-
-export const namespace: Namespace = {
-    name: 'American Bar Association',
-    url: 'americanbar.org',
-    description: 'The American Bar Association is the largest voluntary association of lawyers in the world.',
-    lang: 'en',
-};
